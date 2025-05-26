70279af45ed7cebd9f69c8e29b2686e7b4a7d964
mbero_akoko <mberoakoko@gmail.com>
2025-05-26 22:01:35 +0200

    Added rotation routines

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "src/kinematics/kinetics.hpp"
+
+class from_string {
+    const char* str_;
+public:
+    explicit from_string(const char* str): str_(str) {}
+
+    template<typename type>
+    explicit operator type() {
+        if constexpr (std::is_same_v<type, float>) {return std::stof(str_);}
+        else if constexpr (std::is_same_v<type, double>) {return std::stod(str_);}
+        return std::stoi(str_);
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto [omega_3_hat, theta] { Kinetics::axis_angle_3({1, 2, 3}) };
+    std::cout<<"Theta " <<theta<<"\n";
+    std::cout<<"Axis\n" <<omega_3_hat << "\n";
+    std::cout << Kinetics::vec_3_to_so3(omega_3_hat) << std::endl;
+    auto temp = Kinetics::vec_3_to_so3(omega_3_hat);
+    std::cout << temp * temp << std::endl;
+    auto rotated_matrix = Kinetics::rot_axis(omega_3_hat, theta);
+    std::cout<<"RotatedMatrix\n" <<rotated_matrix << "\n";
+
     return 0;
 }
--- src/kinematics/kinetics.hpp
+++ src/kinematics/kinetics.hpp
@@ -5,4 +5,64 @@
 #ifndef KINETICS_HPP
 #define KINETICS_HPP
 
+#include <bits/stdc++.h>
+#include <eigen3/Eigen/Core>
+#include <oneapi/tbb/task_arena.h>
+
+namespace Kinetics {
+    using Screw = Eigen::Vector<float, 6>;
+    using Twist = Eigen::Vector<float, 6>;
+
+    struct ExponentialCoordinate {
+        Eigen::Vector3f omega_3_hat;
+        float theta;
+
+        auto to_vector() const -> Eigen::Vector3f { return theta * omega_3_hat; }
+        static auto from_vector(const Eigen::Vector3f &v) -> ExponentialCoordinate {
+            return {
+                .omega_3_hat = v.normalized(),
+                .theta = v.norm()
+            };
+        }
+    };
+
+    inline auto rotation_inverse(const Eigen::Matrix3f& rotation_matrix) -> Eigen::Matrix3f {
+        Eigen::Matrix3f inverse = rotation_matrix.transpose();
+        return inverse;
+    };
+
+    inline auto vec_3_to_so3(const Eigen::Vector3f& vector) -> Eigen::Matrix3f {
+        return Eigen::Matrix3f{
+            {
+                {0              , -vector(2),   vector(1)},
+                {vector(2),                 0, -vector(0)},
+                {-vector(1), vector(0),                 0}
+            }
+            };
+    }
+
+    inline auto so3_to_vec_3(const Eigen::Matrix3f& so3) -> Eigen::Vector3f {
+        return Eigen::Vector3f{-so3(0, 1), so3(0, 2), -so3(1, 2)};
+    }
+
+    inline auto axis_angle_3(const Eigen::Vector3f & exponential_3) -> ExponentialCoordinate {
+        return ExponentialCoordinate::from_vector(exponential_3);
+    }
+
+    inline auto matrix_exponent_3(const Eigen::Matrix3f& so3_matrix) -> Eigen::Matrix3f {
+        auto exponential_3 = Kinetics::so3_to_vec_3(so3_matrix);
+        auto [omega_3_hat, theta] = Kinetics::axis_angle_3(exponential_3);
+        const auto omega_skew = Kinetics::vec_3_to_so3(omega_3_hat);
+        const auto expr_1 = ::sin(theta) * omega_skew;
+        const auto expr_2 = (1 - ::cos(theta)) * (omega_skew * omega_skew);
+        return Eigen::Matrix3f::Identity() + expr_1 * expr_2;
+    }
+
+    inline auto rot_axis(const Eigen::Vector3f& omega , const float theta) -> Eigen::Matrix3f {
+        return Kinetics::matrix_exponent_3(
+            Kinetics::vec_3_to_so3(omega * theta)
+        );
+    }
+
+}
 #endif //KINETICS_HPP
